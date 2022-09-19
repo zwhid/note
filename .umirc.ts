52e68487e7cd75f6c59da88b97340d124a2d4ac5
zwh <eueni@163.com>
2022-09-19 16:28:02 +0800

    update

--- .umirc.ts
+++ .umirc.ts
@@ -10,32 +10,79 @@ export default defineConfig({
   base: '/note/',
   publicPath: '/note/',
   navs: [
-    { title: '造轮子学源码', path: '/wheel' },
-    { title: 'Node.js', path: '/node.js' },
-    { title: 'JavaScript', path: '/java-script' },
-    { title: 'Vue2', path: '/vue2' },
-    { title: 'Webpack', path: '/webpack' },
-    { title: '工程化', path: '/engineering' },
-    { title: 'HTTP', path: '/http' },
     {
-      title: '更多',
-      path: '/monitor',
+      title: 'Spring',
+      path: '/spring',
       children: [
-        { title: '前端监控', path: '/monitor' },
-        { title: 'React', path: '/react' },
-        { title: 'CSS', path: '/css' },
-        { title: 'Dom', path: '/browser' },
-        { title: 'TypeScript', path: '/type-script' },
+        { title: 'Spring', path: '/spring' },
+        { title: 'SpringMVC', path: '/spring-mvc' },
+        { title: 'SpringBoot', path: '/spring-boot' },
+        { title: 'Servlet', path: '/servlet' },
+      ],
+    },
+
+    { title: 'Java', path: '/java-se' },
+
+    {
+      title: '数据库',
+      path: '/my-sql',
+      children: [
+        { title: 'MySQL', path: '/my-sql' },
+        { title: 'Redis', path: '/redis' },
+        { title: 'ElasticSearch', path: '/elastic-search' },
+        { title: 'MongoDB', path: '/fe_mongo-db' },
+      ],
+    },
+
+    {
+      title: '架构',
+      path: '/spring-cloud',
+      children: [
+        { title: 'SpringCloud', path: '/spring-cloud' },
+        { title: '商品秒杀', path: '/architect' },
+        { title: 'MQ', path: '/mq' },
+      ],
+    },
+
+    {
+      title: '前端开发',
+      path: '/fe_wheel',
+      children: [
+        { title: '前端轮子', path: '/fe_wheel' },
+        { title: 'NodeJS', path: '/fe_node-js' },
+        { title: 'HTTP', path: '/fe_http' },
+        { title: 'JavaScript', path: '/fe_java-script' },
+        { title: 'Vue', path: '/fe_vue2' },
+        { title: 'React', path: '/fe_react' },
+        { title: 'CSS', path: '/fe_css' },
+        { title: 'Dom', path: '/fe_browser' },
+        { title: 'TypeScript', path: '/fe_type-script' },
+      ],
+    },
+
+    {
+      title: '运维部署',
+      path: '/nginx',
+      children: [
         { title: 'Linux', path: '/linux' },
         { title: 'Nginx', path: '/nginx' },
         { title: 'Docker', path: '/docker' },
-        { title: 'MySQL', path: '/my-sql' },
-        { title: 'MongoDB', path: '/mongo-db' },
-        { title: 'Python', path: '/python' },
+        { title: 'Webpack', path: '/fe_webpack' },
+        { title: '前端自动化构建部署', path: '/fe_cicd' },
+        { title: '前端监控', path: '/fe_monitor' },
+      ],
+    },
+
+    {
+      title: '通用技术',
+      path: '/crawl',
+      children: [
         { title: '爬虫', path: '/crawl' },
         { title: '算法', path: '/algorithm' },
-        { title: '其他', path: '/other' },
-      ]
+        { title: '数据结构', path: '/data-structure' },
+        { title: 'Python', path: '/python' },
+        { title: '工具', path: '/tools' },
+      ],
     },
-  ]
+  ],
 });
--- docs/Docker/aa.md
+++
@@ -1,7 +0,0 @@
-```js
-[webserver]
-172.16.137.137 ansible_ssh_port=22 ansible_ssh_user=root ansible_ssh_pass="123456"
-172.16.137.132 ansible_ssh_port=22 ansible_ssh_user=root ansible_ssh_pass="123456"
-```
-
-172.16.137.137:8081
